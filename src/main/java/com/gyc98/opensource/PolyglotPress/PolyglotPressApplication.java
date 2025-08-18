e1785cc58561526cf0d55ab9a855973729220173
Yuanchen Gao <gyc98@YuanchendeMacBook-Pro.local>
2025-08-18 22:50:41 +0800

    first commit

---
+++ src/main/java/com/gyc98/opensource/PolyglotPress/PolyglotPressApplication.java
@@ -0,0 +1,13 @@
+package com.gyc98.opensource.PolyglotPress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotPressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyglotPressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyc98/opensource/PolyglotPress/PolyglotPressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyc98.opensource.PolyglotPress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotPressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
